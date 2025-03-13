809cecbc127f237bb4aba4332c7186e8d4be132f
BeastTheNinja <134609528+BeastTheNinja@users.noreply.github.com>
2025-03-13 10:00:19 +0100

    opgave 1 #1

--- assets/js/if-then.js
+++ assets/js/if-then.js
@@ -2,39 +2,56 @@
 // variabler du skal bruge i din kode...
 const A = 10;
 const B = 30;
-const C = '30';
+const C = 30;
 const D = 10;
 const E = 40;
 
 // Opgave 1: skriv et expression der viser true når A er lig med B.
-
-if ( /*..dit expression Her*/ ) {
-    console.log('statement true!');
+if (A == B) {
+    console.log(true);
 }
-
-
+else{
+    console.log(false);   
+}
 //opgave 2: skrive en If then statement der vurderer om A er større end B.
-
-
-
-
+if(A>B){
+    console.log(true);   
+}
+else{
+    console.log(false);  
+}
 //opgave 3: Skriv en If then statement der vurderer om A er mindre end eller lig med B
-
-
-
-
-
+if(A<=B){
+    console.log(true);
+    
+}
+else{
+    console.log(false);
+    
+}
 //opgave 4: Skriv en If then statement der bliver FALSE når du sammenligner B og C.
 
-
-
-
-
+if(B === C){
+    console.log(true);
+    
+}
+else{
+    console.log(false);
+    
+}
 //opgave 5: Skriv en If then statement der bliver TRUE når du sammenligner B og C.
+if(B === C){
 
-
-
-
+}
+else{
+        console.log(true);
+        
+    }
 //opgave 6: Skriv en If then statement der sammenligner B = C og E = A hvis B=C skal den blive TRUE. 
-
+if (B == C && E == A) { 
+    console.log(true); 
+}
+ else { 
+    console.log(false); 
+}
 
